9c651c2164c7a5b7e9920ce98b18bed95c53b783
GersonESS <gebhsantos@gmail.com>
2025-06-01 23:12:50 -0300

    a

--- 1_React_Exemplo Material 3 Expressive/00MD3/md3expr/src/UserProfileCard.jsx
+++ 1_React_Exemplo Material 3 Expressive/00MD3/md3expr/src/UserProfileCard.jsx
@@ -1,9 +1,6 @@
 // c:\Repo2024\MD3EmREACT\MD3V2\00Md03\md3expr\src\UserProfileCard.jsx
 import React from 'react';
-// Assuming App.css is imported globally in App.jsx or index.js,
-// where the new styles will be added.
-// If you prefer a separate CSS file for this component:
-// import './UserProfileCard.css';
+import './UserProfileCard.css'; // Importa o CSS específico do componente
 
 function UserProfileCard({ username, email, onLogout }) {
   // Lógica aprimorada para determinar o nome de exibição
